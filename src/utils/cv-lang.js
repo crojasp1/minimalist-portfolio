34cd3d99c6c57cd1f6dcf6423d49f1fdcb786331
Marvin Cely <mardancely@gmail.com>
2024-07-11 18:10:23 -0500

    feat: Include languages component

--- src/utils/cv-lang.js
+++ src/utils/cv-lang.js
@@ -1,5 +1,10 @@
-import cv from '@json/cv.json';
+export default async function (lang = 'en') {
+	let cv = {};
+	try {
+		cv = await import(`../../json/cv-${lang}.json`);
+	} catch (error) {
+		console.error(error);
+	}
 
-export default function (lang = 'en') {
 	return cv;
 }
