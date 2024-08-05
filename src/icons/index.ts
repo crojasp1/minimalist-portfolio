447d8e53cefb819f32258253fe33c3751a93471c
Marvin Cely <mardancely@gmail.com>
2024-08-05 13:04:55 -0500

    Adjust Resume language & use en ans es lang json

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,33 +1,35 @@
-import Github from './Github.astro';
-import Linkedin from './Linkedin.astro';
-import Mail from './Mail.astro';
-import Whatsapp from './Whatsapp.astro';
-import World from './World.astro';
-import Javascript from './Javascript.astro';
-import React from './React.astro';
-import NodeJs from './NodeJs.astro';
 import Angular from './Angular.astro';
-import Sass from './Sass.astro';
-import Typescript from './Typescript.astro';
-import HTML from './HTML.astro';
 import CSS from './CSS.astro';
 import Git from './Git.astro';
+import Github from './Github.astro';
+import HTML from './HTML.astro';
+import Javascript from './Javascript.astro';
+import Language from './Language.astro';
+import Linkedin from './Linkedin.astro';
+import Mail from './Mail.astro';
+import NodeJs from './NodeJs.astro';
+import React from './React.astro';
+import Sass from './Sass.astro';
+import Typescript from './Typescript.astro';
+import Whatsapp from './Whatsapp.astro';
+import World from './World.astro';
 
 const icons = {
+	angular: Angular,
+	css: CSS,
+	git: Git,
 	github: Github,
+	html: HTML,
+	language: Language,
 	linkedin: Linkedin,
 	mail: Mail,
-	whatsapp: Whatsapp,
-  world: World,
-  javascript: Javascript,
-  react: React,
 	nodejs: NodeJs,
-	angular: Angular,
 	sass: Sass,
 	typescript: Typescript,
-	html: HTML,
-	css: CSS,
-	git: Git
+	whatsapp: Whatsapp,
+  javascript: Javascript,
+  react: React,
+  world: World,
 };
 
 
